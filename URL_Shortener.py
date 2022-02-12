52d160b4f19593dddd675e7a55fdc65260ae2fea
k1borG <kiborg66666666@yandex.ru>
2022-02-12 12:10:40 +0300

    Try 1

--- URL_Shortener.py
+++ URL_Shortener.py
@@ -1,1 +1,7 @@
 # It's my first day of work on GitHub
+import bitly_api
+BITLY_ACCESS_TOKEN ="YOUR_ACCESS_TOKEN"
+access = bitly_api.Connection(access_token = BITLY_ACCESS_TOKEN)
+full_link = input()
+short_url = access.shorten(full_link)
+print('Short URL = ',short_url['url'])
